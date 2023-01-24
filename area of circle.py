c6ce5d3af551be7024916b86dd5ce5b319d629a5
kevosmart <123382433+kevosmart@users.noreply.github.com>
2023-01-24 07:16:22 +0300

    Add files via upload
    area of circle

---
+++ area of circle.py
@@ -0,0 +1,6 @@
+#Calculate the area of the given circle using π
+
+π = 3.14  
+Radius = float (input ("Please enter the radius of the given circle: "))  
+area_of_the_circle = π * Radius * Radius  
+print (" The area of the given circle is: ", area_of_the_circle) 

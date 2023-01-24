aa7cb2938d91d6ed3bc4deca07bea3f5ff8408ee
kevosmart <123382433+kevosmart@users.noreply.github.com>
2023-01-24 07:28:07 +0300

    Add files via upload
    volume of sphere

---
+++ volume of sphere.py
@@ -0,0 +1,6 @@
+#Calculate the volume of a sphere using pie
+
+Radius = float (input ("Please enter the radius of the given circle: "))  
+pie=3.14285714286  
+volume=(4.0/3.0)*pie*(Radius*Radius*Radius)  
+print("volume of the sphere="+str(volume))
